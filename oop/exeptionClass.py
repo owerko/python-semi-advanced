1f49c323eb657e0d086a808de7774d21f9e94467
Tomek <owerko@gmail.com>
2019-07-17 15:37:52 +0200

    Implement turnament

---
+++ oop/exeptionClass.py
@@ -0,0 +1,8 @@
+class NameToShortExeption(BaseException):
+    pass
+
+class WrongRanking(BaseException):
+    pass
+
+class WrongResult(BaseException):
+    pass
---
+++ oop/scripts/main.py
@@ -0,0 +1,3 @@
+from oop.sports import *
+if __name__ == '__main__':
+    pl = Player('aaaa',1222)
