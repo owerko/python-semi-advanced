47e2951a62013834dc73ae0af489cf20beb20f5e
owerko <50355739+owerko@users.noreply.github.com>
2019-07-17 13:48:29 +0200

    Update master (#1)
    * Implemented restaurant project
    * Fetched with upstream

--- restaurant/scripts/main.py
+++ restaurant/scripts/main.py
@@ -1,6 +1,21 @@
-
-
-
+from restaurant.cash_desk import CashDesk
+from restaurant.kitchen import Kitchen
+from restaurant.give_away import GiveAway
+from restaurant.manager import Manager
+from restaurant.order import Order
 
 if __name__ == '__main__':
-    pass
+    cash_desk = CashDesk(None)
+    kitchen = Kitchen(None)
+    give_away = GiveAway(None)
+
+    manager = Manager(cash_desk, kitchen, give_away)
+
+    cash_desk.manager = manager
+    kitchen.manager = manager
+    give_away.manager = manager
+
+    #KONIEC SETUPU
+
+    cash_desk.new_order(Order('Burger'))
+    cash_desk.new_order(Order('Pizza'))
